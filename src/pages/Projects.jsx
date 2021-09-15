9f0fbcd2f398cf28a532424bda80fb403cef65f7
Marco Galindo <marco1408@gmail.com>
2021-09-14 21:11:56 -0300

    adicionando projetos pixels art, todo list e shopping cart

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -4,6 +4,9 @@ import '../styles/Projects.scss';
 import recipesAppImg from '../assets/images/recipes-app.png';
 import triviaGameImg from '../assets/images/trivia-game.png';
 import githubUserImg from '../assets/images/github-user-search.png';
+import todoList from '../assets/images/todo-list.png';
+import pixelsArt from '../assets/images/pixels-art.png';
+import shoppingCart from '../assets/images/shopping-cart.png';
 
 export default class Projects extends Component {
   render() {
@@ -14,7 +17,40 @@ export default class Projects extends Component {
           <h2>projetos</h2>
           <div id="inner-projects">
             <div>
-              <a class="project" id="github-redux-app" href="https://marcoglnd.github.io/github-redux-app" target="_blank" rel="noreferrer">
+              <a class="project" href="https://marcoglnd.github.io/pixels-art/" target="_blank" rel="noreferrer">
+                <img src={ pixelsArt } alt="Pixels Art" />
+                <div>
+                  <div>
+                    <h3>Pixels Art</h3><h4>| Abril/2021</h4>
+                  </div>
+                  <p>Aplicação web onde você pode criar uma arte pixelada, interagindo com uma paleta de cores e uma tela em branco. Feito com HTML, CSS e JS.</p>
+                </div>
+              </a>
+            </div>
+            <div>
+              <a class="project" href="https://marcoglnd.github.io/todo-list/" target="_blank" rel="noreferrer">
+                <img src={ todoList } alt="Todo List" />
+                <div>
+                  <div>
+                    <h3>To Do List</h3><h4>| Maio/2021</h4>
+                  </div>
+                  <p>Lista de tarefas customizável, onde o usuário pode adicionar, remover e salvar tarefas diversas. Feito com HTML, CSS e JS.</p>
+                </div>
+              </a>
+            </div>
+            <div>
+              <a class="project" href="https://marcoglnd.github.io/shopping-cart/" target="_blank" rel="noreferrer">
+                <img src={ shoppingCart } alt="Shopping Cart" />
+                <div>
+                  <div>
+                    <h3>Shopping Cart</h3><h4>| Maio/2021</h4>
+                  </div>
+                  <p>Página web com carrinho de compras criada a partir de uma requisição da API do Mercado Livre. Criado com HTML, CSS e JS, usando requisições API.</p>
+                </div>
+              </a>
+            </div>
+            <div>
+              <a class="project" href="https://marcoglnd.github.io/github-redux-app" target="_blank" rel="noreferrer">
                 <img src={ githubUserImg } alt="Github User Search" />
                 <div>
                   <div>
@@ -25,7 +61,7 @@ export default class Projects extends Component {
               </a>
             </div>
             <div>
-              <a class="project" id="trivia-game" href="https://marcoglnd.github.io/trivia-game" target="_blank" rel="noreferrer">
+              <a class="project" href="https://marcoglnd.github.io/trivia-game" target="_blank" rel="noreferrer">
                 <img src={ triviaGameImg } alt="Trivia Game" />
                 <div>
                   <div>
@@ -36,7 +72,7 @@ export default class Projects extends Component {
               </a>
             </div>
             <div>
-              <a class="project" id="recipes-app" href="https://marcoglnd.github.io/recipes-app/" target="_blank" rel="noreferrer">
+              <a class="project" href="https://marcoglnd.github.io/recipes-app/" target="_blank" rel="noreferrer">
                 <img src={ recipesAppImg } alt="Recipes App" />
                 <div>
                   <div>
